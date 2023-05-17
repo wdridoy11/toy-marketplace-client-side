010e316bbf300f52fe2ea6ef9f29bce349f8098a
Ridoy Sharif <developersridoy@gmail.com>
2023-05-17 20:53:44 +0600

    react, react router, tailwindcss install successful and create some folder

---
+++ src/routes/routes/routes.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {createBrowserRouter} from "react-router-dom";
+import Main from '../../layout/Main';
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Main></Main> ,
+      children:[
+        {
+            path:"/",
+            element:<></>
+        }
+      ]
+    },
+  ]);
+
+
+export default router
