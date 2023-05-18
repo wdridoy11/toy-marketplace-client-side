0131f638938c81c9953a0ab68d745bc3f5059933
Ridoy Sharif <developersridoy@gmail.com>
2023-05-18 18:38:24 +0600

    hero section design

--- src/pages/home/hero/Hero.jsx
+++ src/pages/home/hero/Hero.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import heroImage from '../../../assets/hero-02.jpg'
 const Hero = () => {
   return (
     <div>
-      <div>
-        
+      <div className="hero min-h-screen" style={{ backgroundImage: `url(${heroImage})`}}>
+        <div className="hero-overlay bg-opacity-30"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-xl">
+            <h1 className="mb-5 text-5xl font-bold">Engineering kits</h1>
+            <p className="mb-5"> 
+                Engineering kits are educational tools designed to introduce and engage individuals, particularly 
+                children and students, in the field of engineering. These kits typically contain a variety of 
+                components, materials, and instructions that allow users to build and experiment with various 
+                engineering concepts and principles.
+            </p>
+            <button className='py-3 px-10 bg-orange-400 text-white font-semibold text-base'>Get Started</button>
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/pages/home/home/Home.jsx
+++ src/pages/home/home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Hero from '../hero/Hero'
 import About from '../../about/About'
+import UserTitle from '../../../components/shared/useTitle/UserTitle'
 
 const Home = () => {
+  UserTitle("Home")
   return (
     <div>
       <Hero></Hero>
