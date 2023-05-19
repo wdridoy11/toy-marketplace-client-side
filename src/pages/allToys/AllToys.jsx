c7ab61fad7f1a2df000608f93bcf21a11dc4d0cb
Ridoy Sharif <developersridoy@gmail.com>
2023-05-19 11:54:04 +0600

    search system add successful

--- src/pages/allToys/AllToys.jsx
+++ src/pages/allToys/AllToys.jsx
@@ -3,7 +3,7 @@ import AllToy from './AllToy'
 
 const AllToys = () => {
   const [allToysData,setAllToysData] = useState([]);
-  
+  const [searchText, setSearchText] = useState("");
   useEffect(()=>{
     fetch(`http://localhost:5000/toyMarketplace`)
     .then((res)=>res.json())
@@ -11,11 +11,28 @@ const AllToys = () => {
       setAllToysData(data)
     })
   },[])
+
+  const handleSearch=()=>{
+    fetch(`http://localhost:5000/toySearchByName/${searchText}`)
+    .then((res)=>res.json())
+    .then((data)=>{
+      setAllToysData(data)
+    })
+  }
+
   return (
     <div className='py-20'>
       <div className='container mx-auto'>
-        <div>
-          <input type="search" name="search" id="search" />
+        <div className='text-center mb-10'>
+          <input 
+              type="search" 
+              name="search" 
+              id="search" 
+              onChange={(e)=>setSearchText(e.target.value)}
+              className='input input-bordered w-full max-w-md' 
+              placeholder='Search here...' 
+          />
+          <button onClick={handleSearch} className='py-3 rounded-lg px-5 bg-orange-400 text-white font-semibold text-base ml-2'>Search</button>
         </div>
         <div className='grid grid-cols-4 gap-7'>
           {allToysData.map((allToy)=><AllToy key={allToy._id} allToy={allToy}></AllToy>)}
