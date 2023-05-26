ac7308530ed942433da18c4a61d68a0d1728a7b1
Ridoy Sharif <developersridoy@gmail.com>
2023-05-26 17:09:39 +0600

    add a comment

--- src/utils/firebase/firebase.config.js
+++ src/utils/firebase/firebase.config.js
@@ -12,6 +12,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId,
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
